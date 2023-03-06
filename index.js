33aa2bf91e868f1d3c49060b72dc6bf7b85b4d02
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-06 14:30:34 +0300

    adding platform

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const doodler = document.createElement("div");
   let doodlerLeftSpace = 50;
   let doodlerBottomSpace = 150;
+  let platformCount = 5;
+  let platforms = [];
   let isGameOver = false;
 
   function createDoodler() {
@@ -12,9 +14,34 @@ document.addEventListener("DOMContentLoaded", () => {
     doodler.style.bottom = doodlerBottomSpace + "px";
   }
 
+  class Platform {
+    constructor(newPlatBottom) {
+      this.left = Math.random() * 315;
+      this.bottom = newPlatBottom;
+      this.visual = document.createElement("div");
+
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+      grid.appendChild(visual);
+    }
+  }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platGap = 600 / platformCount;
+      let newPlatBottom = 100 + i * platGap;
+      let newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform);
+      console.log(platforms);
+    }
+  }
+
   function start() {
     if (!isGameOver) {
       createDoodler();
+      createPlatforms();
     }
   }
   start();
